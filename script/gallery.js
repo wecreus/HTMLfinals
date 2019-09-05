445c56908da4cba33e3b2dca4687f0b83fb08893
wecreus <wecreus@gmail.com>
2019-09-06 02:09:31 +0300

    gallery is done

---
+++ script/gallery.js
@@ -0,0 +1,18 @@
+let slideIndex = 1;
+
+window.onload = function () { showDivs(slideIndex); };
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("imageItself");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
